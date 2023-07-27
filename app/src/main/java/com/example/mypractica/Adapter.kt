a11bd07c353b0b238570f5a055668ae6d450b311
ginger cea <gingercea@gmail.com>
2023-07-26 20:32:13 -0400

    Inicio proyecto se implementa layaout desing y boton con edit text con ids

---
+++ app/src/main/java/com/example/mypractica/Adapter.kt
@@ -0,0 +1,4 @@
+package com.example.mypractica
+
+class Adapter {
+}
--- app/src/main/java/com/example/mypractica/MainActivity.kt
+++ app/src/main/java/com/example/mypractica/MainActivity.kt
@@ -2,7 +2,16 @@ package com.example.mypractica
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//[]Recycler view
+//[]Room
+//[] dao
+//[] base datos
+//[] entity
+//[] crear interfaz
+//[] fragmento listado
+//[] fragmento agregar
+//[] navigation
+// **
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
